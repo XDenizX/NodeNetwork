611f63f749b7d4304fb8d81773da0bd4bb7d5168
XDenizX <denizik1@mail.ru>
2022-08-03 16:38:31 +0300

    Added some properties for comment box view-model.

---
+++ NodeNetwork/ViewModels/CommentBoxViewModel.cs
@@ -0,0 +1,43 @@
+﻿using NodeNetwork.Views;
+using ReactiveUI;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NodeNetwork.ViewModels
+{
+    public class CommentBoxViewModel : ReactiveObject
+    {
+        static CommentBoxViewModel()
+        {
+            NNViewRegistrar.AddRegistration(() => new CommentBoxView(), typeof(IViewFor<CommentBoxViewModel>));
+        }
+
+        #region Parent
+        /// <summary>
+        /// The network that contains this node
+        /// </summary>
+        public NetworkViewModel Parent
+        {
+            get => _parent;
+            internal set => this.RaiseAndSetIfChanged(ref _parent, value);
+        }
+        private NetworkViewModel _parent;
+        #endregion
+
+        #region Description
+        public string Description
+        {
+            get => _description;
+            internal set => this.RaiseAndSetIfChanged(ref _description, value);
+        }
+        private string _description;
+        #endregion
+
+        public CommentBoxViewModel()
+        {
+
+        }
+    }
+}
